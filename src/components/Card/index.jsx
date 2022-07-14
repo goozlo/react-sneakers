db90c24c710fc05c228fd13e92f42d94011af825
Nikita Leonov <nikitaleonov00@mail.ru>
2022-07-14 20:11:37 +0300

    Добавил пропсы и css-модули

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Card.module.sass";
 
 const Card = (props) => {
   return (
-    <div className="card">
+    <div className={styles.card}>
       <div className="favorite">
         <img
           width={32}
@@ -11,14 +11,14 @@ const Card = (props) => {
           alt="unliked"
         />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between  align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={11} height={11} src="/img/btn-plus.svg" alt="plus" />
         </button>
       </div>
